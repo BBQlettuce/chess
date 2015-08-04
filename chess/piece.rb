d67054ba596ca33bd5cadae7c200f73a5c1d5df8
Oscar & Melissa <OM@appacademy.io>
2015-08-04 13:58:44 -0400

    complete moves methods for sliding and stepping pieces

--- chess/piece.rb
+++ chess/piece.rb
@@ -9,15 +9,7 @@ class Piece
   end
 
   def moves(pos)
-    move_dirs.each do |(x,y)|
-      new_x = pos[0] + x
-      new_y = pos[1] + y
-      until new_x > 7 || new_y > 7
-        moves << [new_x, new_y]
-        new_x += x
-        new_y += y
-      end
-    end
+
   end
 
 end
@@ -27,9 +19,34 @@ class SlidingPiece < Piece
   VERTICALS = [[1, 0], [-1, 0], [0, 1], [0, -1]]
   DIAGONALS = [[1, 1], [-1, 1], [1, -1], [-1, -1]]
 
+  def moves(pos)
+    move_dirs.each do |(x,y)|
+      new_x = pos[0] + x
+      new_y = pos[1] + y
+      until !new_x.between?(0,7) || !new_y.between?(0,7)
+        moves << [new_x, new_y]
+        new_x += x
+        new_y += y
+      end
+    end
+  end
+
 end
 
 class SteppingPiece < Piece
+  KNIGHT_STEPS = [
+    [-1, -2], [-1, 2], [1, -2], [1, 2],
+    [-2, -1], [-2, 1], [2, -1], [2, 1]]
+  KING_STEPS =
+    [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
+
+  def moves(pos)
+    move_dirs.each do |(x,y)|
+      new_x = pos[0] + x
+      new_y = pos[1] + y
+      moves << [new_x, new_y] if new_x.between?(0,7) && new_y.between?(0,7)
+    end
+  end
 
 end
 
@@ -59,7 +76,9 @@ class Queen < SlidingPiece
 end
 
 class Knight < SteppingPiece
-
+  def move_dirs
+    KNIGHT_STEPS
+  end
 end
 
 class King < SteppingPiece
